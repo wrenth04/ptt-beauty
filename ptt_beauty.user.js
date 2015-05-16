d2bc302ca40822857999ae15b26f1ac02b04e9c7
Wei <wrenth04@gmail.com>
2015-05-17 02:41:35 +0800

    add bookmarklet and userscript

---
+++ ptt_beauty.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        ptt beauty
+// @namespace   wei
+// @include     https://www.ptt.cc/bbs/*
+// @require		https://bitbucket.org/wrenth04/ptt-beauty/raw/master/src/ptt-beauty.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
